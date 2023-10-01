d0d77b50be1d41e19b65de034bc1679ef0b05167
Andrew Rosario <71676291+andrew-rosario@users.noreply.github.com>
2023-10-01 17:45:57 -0600

    Passed most tests

--- server.py
+++ server.py
@@ -1,7 +1,5 @@
 #  coding: utf-8 
-import socketserver,os
-from requests import get
-import json
+import socketserver, os
 
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
 # 
@@ -28,48 +26,85 @@ import json
 
 # try: curl -v -X GET http://127.0.0.1:8080/
 
-BASE_URL = './www/'
-OK_RESPONSE = b'HTTP/1.1 200 OK\n'
-NOT_EXIST_RESPONSE =b'HTTP/1.1 404 Not Found\n'
+ROOT_FOLDER = 'www'
+OK_RESPONSE = 'HTTP/1.1 200 OK\n'
+NOT_EXIST_RESPONSE = 'HTTP/1.1 404 Not Found\n'
+CONTENT_TYPE_HEADER = 'Content-Type: '
+HTML_CONTENT_TYPE = 'text/html; charset=utf-8\n'
+CSS_CONTENT_TYPE = 'text/css\n'
 DEBUG_RESPONSE = OK_RESPONSE
 
 
+class NotAFileException(Exception):
+    pass
+
+
 class MyWebServer(socketserver.BaseRequestHandler):
-    
+
     def handle(self):
         self.data = self.request.recv(1024).strip()
-        print ("Got a request of: %s\n" % self.data)
-        
-        #self.request.sendall(bytearray("OK",'utf-8'))
-        base_response = b'HTTP/1.1 200 OK\n'
-        
+        print("Got a request of: %s\n" % self.data)
+
+        # self.request.sendall(bytearray("OK",'utf-8'))
+
         request_headers = self.data.decode().split("\r\n")
         print(request_headers)
         http_request = request_headers[0].split(" ")
+        response = DEBUG_RESPONSE
         if http_request[0] == "GET":
-            path_to_thing = os.path.join(BASE_URL,http_request[1])
-            self.handle_file_request(http_request[1])
-            
-        
+            path_to_thing = 'www' + http_request[1]
+            response = self.handle_file_request(path_to_thing)
+            print(response)
+        else:
+            response = "HTTP/1.1 405 Method Not Allowed"
+        #response.rstrip('\n')
         # with open(BASE_URL,'r') as html_page:
         #     html_content = html_page.read()
         # html_content = html_content.encode('ascii')
         # response = ok_base_response + content_type_header + html_content
-        
-        self.request.sendall(DEBUG_RESPONSE)
 
-    def handle_file_request(self,path_to_file):
+        self.request.sendall(response.encode("utf-8"))
+
+    def handle_file_request(self, path_to_file):
         request_to_send = []
-        if path_to_file == :
-            if os.path.exists(os.path.join(BASE_URL,"index.html")):
+        print(f"Path: {path_to_file}")
+        file_path_components = os.path.split(path_to_file)
+        print(f"File component index 1: {file_path_components[1]}")
+        if not file_path_components[1]:  # simply going into a directory
+            print(f"Directory. Does index exist")
+            index_path = os.path.join(file_path_components[0], "index.html")
+            index_exists = os.path.isfile(index_path)
+            print(f"Path joined: {index_path}")
+            print(f"Index exists: {index_exists}")
+            if index_exists:
                 request_to_send.append(OK_RESPONSE)
+                request_to_send.append(CONTENT_TYPE_HEADER + HTML_CONTENT_TYPE + "\n")
             else:
                 request_to_send.append(NOT_EXIST_RESPONSE)
-        elif os.path.splitext(os.path.join(BASE_URL,file_requested))[1] == ".html":
-            pass
-        
-        
-    
+            request_to_send.append(self.parse_web_file(index_path))
+        elif os.path.exists(path_to_file):
+            request_to_send.append(OK_RESPONSE)
+            if os.path.splitext(file_path_components[1])[1] == ".html":
+                request_to_send.append(CONTENT_TYPE_HEADER + HTML_CONTENT_TYPE + "\n")
+                request_to_send.append(self.parse_web_file(path_to_file))
+                #request_to_send.append(self.add_css_files(path_to_file))
+            elif os.path.splitext(file_path_components[1])[1] == ".css":
+                request_to_send.append(CONTENT_TYPE_HEADER + CSS_CONTENT_TYPE + "\n")
+                request_to_send.append(self.parse_web_file(path_to_file))
+        else:
+            request_to_send.append(NOT_EXIST_RESPONSE)
+        return ''.join(request_to_send)
+
+    def parse_web_file(self, file_path):
+        try:
+            if not os.path.isfile(file_path) or os.path.splitext(file_path)[1] != ".html" or os.path.splitext(file_path)[1] != ".css":
+                raise NotAFileException
+        except NotAFileException:
+            print("Not a HTML/CSS file.")
+
+        with open(file_path, 'r') as file:
+            file_to_send = file.read()
+        return file_to_send
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
